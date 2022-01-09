1e20eda93899916b06c39e6d64335fbabb48ebd2
raissonls <raissonls@hotmail.com>
2022-01-09 13:06:48 -0300

    Finalizei o curso de JS assincrono, aprendendo a fazer chamados de APIs e utilizanod isso na pratica criando um site que traz imagens de gatos aleatorios

---
+++ Desafio-JS-assincrono/assets/js/script.js
@@ -0,0 +1,25 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const catBotão = document.getElementById('change-cat');
+const catImagem = document.getElementById('cat');
+
+
+
+const getCats = async () => {
+    try {
+        const data = await fetch(BASE_URL);
+        const json = await data.json();
+    
+        return json.webpurl;
+    } catch (error) {
+        console.log(error.message);
+    }
+
+};
+
+const loadImg = async () => {
+    catImagem.src = await getCats();
+}
+
+catBotão.addEventListener('click', loadImg);
+
+loadImg();
